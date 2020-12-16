54a74e840a1d40e0ec271cbc03a3230c709e150f
Mac <codyjseals@gmail.com>
2020-12-16 10:29:32 -0500

    Created file for generating and combining EF Core migration scripts so DB Deployment can be done through a single SQL Script / Artifact

---
+++ scripts/generate_sql_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Parameters PROJECT_PATH, OUTPUT_DIR, SCRIPT_NAME are provided as environment variables in the azure pipeline task that executes this script
+# Generate IdentityContext Migration SQL Script
+dotnet ef migrations script --context IdentityContext --idempotent --output $OUTPUT_DIR/identity.sql --project $PROJECT_PATH
+
+# Create Unified SQL Script and Remove the Individual Scripts
+cat $OUTPUT_DIR/*.sql > $OUTPUT_DIR/$SCRIPT_NAME && (find $OUTPUT_DIR -type f ! -name $SCRIPT_NAME -delete)
