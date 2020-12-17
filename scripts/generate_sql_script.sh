dfc22d469ae56481a68e94568e2e7826618e3a43
MacBeeVR <codyjseals@gmail.com>
2020-12-17 16:06:53 -0500

    d

--- scripts/generate_sql_script.sh
+++ scripts/generate_sql_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Parameters PROJECT_PATH, OUTPUT_DIR, SCRIPT_NAME are provided as environment variables in the azure pipeline task that executes this script
 # Generate IdentityContext Migration SQL Script
-dotnet ef migrations script --idempotent --no-build --context IdentityContext --output $OUTPUT_DIR/identity.sql --project $PROJECT_PATH
+dotnet ef migrations script --idempotent --context IdentityContext --output $OUTPUT_DIR/identity.sql --project $PROJECT_PATH
 
 # Create Unified SQL Script and Remove the Individual Scripts
 cat $OUTPUT_DIR/*.sql > $OUTPUT_DIR/$SCRIPT_NAME && (find $OUTPUT_DIR -type f ! -name $SCRIPT_NAME -delete)
